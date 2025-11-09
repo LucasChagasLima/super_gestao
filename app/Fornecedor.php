ca53119c2b1344e843818144c919a7137123f0b8
Lucas Lima <lsclima@sga.pucminas.br>
2025-11-09 12:09:32 -0300

    ajustes nos dados

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     //
+
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+
 }
+
